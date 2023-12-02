6063c7cf1b6cf563e0d0c03de5eb9818b1301959
Himanshusaini12 <himanshusaini962001@gmail.com>
2023-12-02 17:44:06 +0530

    d

--- src/App.jsx
+++ src/App.jsx
@@ -5,34 +5,60 @@ import './App.css';
 function App() {
   const [count, setCount] = useState(0);
   const [messages, setMessages] = useState([]);
+  const [searchTerm, setSearchTerm] = useState('');
+  const [originalMessages, setOriginalMessages] = useState([]);
 
   useEffect(() => {
-    // Fetch data from your Express.js server
     axios.get('https://tgscraper.onrender.com/msg')
-      .then(response => setMessages(response.data))
+      .then(response => {
+        setOriginalMessages(response.data);
+        const filteredMessages = response.data.filter(message =>
+          message.content.toLowerCase().includes(searchTerm.toLowerCase())
+        );
+        setMessages(filteredMessages);
+      })
       .catch(error => console.error('Error fetching data:', error));
-  }, []);
+  }, [searchTerm]);
 
   return (
-    <div>
+    <div className="app-container">
+      {/* <div className="search-container">
+        <input
+          type="text"
+          placeholder="Search for content..."
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+        />
+      </div> */}
+
+      <div class="searchBox">
+
+<input class="searchInput"type="text" name="" placeholder="Search" onChange={(e) => setSearchTerm(e.target.value)} value={searchTerm}/>
+<button class="searchButton" href="#">
+    <i class="material-icons">
+        search
+    </i>
+</button>
+</div>
       
-        
-      
-      <div className="messages">
-        <h2>Telegram Messages</h2>
-        {
-          console.log(messages)
-        }
-        <ul>
-          {messages.map(message => (
-            <li key={message._id}>
-              
-              <strong>Content:</strong> {message.content}
-            </li>
-          ))}
+      <div className="messages-container">
+        <h2> ----------</h2>
+        <ul className="message-list">
+          {searchTerm !== '' ? (
+            messages.map(message => (
+              <li key={message._id}>
+                <strong>Content:</strong> {message.content}
+              </li>
+            ))
+          ) : (
+            originalMessages.map(message => (
+              <li key={message._id}>
+                <strong>Content:</strong> {message.content}
+              </li>
+            ))
+          )}
         </ul>
       </div>
-     
     </div>
   );
 }
