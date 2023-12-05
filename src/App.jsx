ce065529862d585f5169a3a4d35d89b305e35ef3
Himanshusaini12 <himanshusaini962001@gmail.com>
2023-12-05 16:52:54 +0530

    s

--- src/App.jsx
+++ src/App.jsx
@@ -104,9 +104,7 @@ function App() {
 
       <div className="messages-container">
         <h2>Telegram Messages</h2>
-        <div className="message-count">
-          <p>Total Messages: {totalMessages}</p>
-        </div>
+        
 
         {loading ? (
           <p>Loading...</p>
